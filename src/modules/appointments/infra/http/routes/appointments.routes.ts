750b33ec58393bcd4737cd6957e43313367ccfe2
leonaardomuller <leonaardo.muller@gmail.com>
2021-04-09 15:11:11 -0300

    FINALIZANDO BACK-END DO APP: Enviando notificações

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsControler = new AppointmentsController();
+const providerAppointmentsControler = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsControler.create);
+
+appointmentsRouter.get('/me', providerAppointmentsControler.index);
 
 export default appointmentsRouter;
