c26663a3d7d765531553dabe783be351077026f6
Leonardo Muller <leonaardo.muller@gmail.com>
2021-04-13 23:35:53 -0300

    Refatorando testes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -29,13 +29,12 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     });
   }
-
+  // console.log(err);
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
   });
 });
-
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
